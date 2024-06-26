6faee0febfde5bba4fbf5bee6e20f84a0b5392d5
Pradip Parmar <prince.soamedia@gmail.com>
2024-06-26 16:56:09 +0530

    added handling for social signup zoho handling

--- satellite/console/consoleweb/consoleapi/socialmedia/x.go
+++ satellite/console/consoleweb/consoleapi/socialmedia/x.go
@@ -69,7 +69,7 @@ func RedirectURL(t string, zohoInsert bool) (string, error) {
 	}
 
 	if zohoInsert {
-		conf.RedirectURL += "?zoho-insert"
+		conf.RedirectURL += "/zoho"
 	}
 
 	requestUrl := conf.AuthCodeURL(state,
