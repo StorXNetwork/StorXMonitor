6fa8504afd095a44ad0ec07f7df7e9fa7c95c291
Pradip Parmar <prince.soamedia@gmail.com>
2024-08-30 12:04:15 +0530

    Added resources API

--- satellite/console/consoleweb/staticapi/staticapis.go
+++ satellite/console/consoleweb/staticapi/staticapis.go
@@ -14,9 +14,17 @@ var blogList []byte
 //go:embed user-guideline.html
 var userGuideline []byte
 
+//go:embed resources.json
+var resources []byte
+
 func HandleUserGuideline(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	w.Write(userGuideline)
+}
+
+func HandleResources(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resources)
 }
 
 func HandlePaymentPlans(w http.ResponseWriter, r *http.Request) {
